3fdeb4179a420647c34d4f9575238fa06b88c8d5
Arth T <arththakkar@gmail.com>
2016-02-18 22:46:01 +0530

    Static Home Page added

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,7 @@ module Elyxx
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.assets.precompile += %w(.svg .eot .woff .ttf)
+    config.assets.paths << "#{Rails.root.to_s}/app/assets/fonts"
   end
 end
