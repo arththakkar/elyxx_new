ba852a0aeae81e7097bef6df43f6309ba5a8d178
Arth T <arththakkar@gmail.com>
2016-02-22 17:19:34 +0530

    JS removed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
-//= require plugins
-//= require functions
+// require turbolinks
+// require plugins
+// require functions
 // require_tree .
