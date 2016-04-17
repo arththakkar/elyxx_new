85e459167d83324ed266bbe3ec2c8c8f631b0a92
Arth T <arththakkar@gmail.com>
2016-04-17 21:53:22 +0530

    JS added

---
+++ app/assets/javascripts/custom.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+function add_current_class(){
+	alert("Hi");
+	var selector = '#primary-menu ul li';
+
+	$(selector).on('click', function(){
+	    $(selector).removeClass('active');
+	    $(this).addClass('active');
+	});
+}
+});
